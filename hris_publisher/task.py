dd83a13d9f78c0d697565d62ad934c2dfaaaa8da
andrewkrug <andrewkrug@gmail.com>
2018-01-25 12:18:58 -0800

    first pass at refactor to use http api

--- hris_publisher/task.py
+++ hris_publisher/task.py
@@ -4,7 +4,6 @@ import json
 import logging
 
 
-
 from cis.publisher import ChangeDelegate
 from cis.settings import get_config
 
@@ -25,20 +24,16 @@ class CISTask(object):
             'id': config('publisher_name', namespace='cis')
         }
 
-    def prep(self):
-        current_group_list = self.get_groups_for_record()
-        cleaned_group_list = self.clean_hris_assertions(current_group_list)
-        self.reintegrate_groups(cleaned_group_list)
-        return self.construct_profile()
-
     def send(self, data):
         cis_change = ChangeDelegate(self.publisher, {}, data)
         cis_change.boto_session = self.boto_session
 
+        vault_record['groups'] = hris_groups
+
         event = {
             'profile': base64.b64encode(cis_change._prepare_profile_data()).decode(),
             'publisher': {'id': 'hris'},
-            'signature': {}
+            'signature': None
         }
 
         encrypted_profile_data = json.loads(base64.b64decode(event.get('profile')))
@@ -55,32 +50,3 @@ class CISTask(object):
         )
 
         return result
-
-    def get_groups_for_record(self):
-        return self.vault_record.get('groups')
-
-    def clean_hris_assertions(self, current_group_list):
-        cleaned_group_list = []
-        for group in current_group_list:
-            # Remove existing groups with hris_ attrs from assertions.
-            if group.find('hris_') == 0:
-                logger.info('Removing current hris_ assertion from {user} for group : {group}'.format(
-                        user=self.vault_record.get('primaryEmail'),
-                        group=group
-                    )
-                )
-            else:
-                cleaned_group_list.append(group)
-        return cleaned_group_list
-
-    def reintegrate_groups(self, cleaned_group_list):
-        # Add the new assumptions we made about the user to the current grouplist
-        superset_of_groups = cleaned_group_list + self.hris_groups
-        self.vault_record['groups'] = superset_of_groups
-
-        logger.info('Groups reintegrated for user: {user}'.format(user=self.vault_record.get('primaryEmail')))
-
-    def construct_profile(self):
-        logger.info('Sending profile to CIS for: {user}'.format(user=self.vault_record.get('primaryEmail')))
-        data = self.vault_record
-        return data
